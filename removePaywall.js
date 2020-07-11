12c0bfcfc36314c2b3dea2da8a3ae7f6073eb7b2
Sam Sheffres <samsheff@yandex.ru>
2020-07-11 11:45:24 -0700

    remove recipe box popup that doesnt disapear

--- removePaywall.js
+++ removePaywall.js
@@ -1,7 +1,7 @@
 $("#appContainer").remove();
 
 var styleElement = document.createElement("style");
-styleElement.innerHTML = ".nytc---modal-window---noScroll { overflow: inherit; }";
+styleElement.innerHTML = ".nytc---modal-window---noScroll { overflow: inherit; } .nytc---popover---popoverActive { display: none !important; };";
 document.body.appendChild(styleElement);
 
 // Stub SetTimeout function so it doesnt redirect us from the page
