cc432fa59663eb01d873f6244266ef53d156c8a8
Sam Sheffres <samsheff@yandex.ru>
2020-07-11 11:31:56 -0700

    Add Redirect Block

--- removePaywall.js
+++ removePaywall.js
@@ -3,3 +3,8 @@ $("#appContainer").remove();
 var styleElement = document.createElement("style");
 styleElement.innerHTML = ".nytc---modal-window---noScroll { overflow: inherit; }";
 document.body.appendChild(styleElement);
+
+window.onbeforeunload = function () {
+    console.log("Redirect Blocked!");
+    return false;
+};
